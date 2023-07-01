6ace19f15d9ab6991e86d640b87ddfa486f389ac
darshan-balar2400 <90057670+darshan-balar2400@users.noreply.github.com>
2023-07-01 10:57:02 +0530

    ready for deploy

--- src/app.js
+++ src/app.js
@@ -6,7 +6,20 @@ const app = express();
 // paths
 const staticPath = path.join(__dirname,"../public");
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
+
+
+app.get("/",(req,res)=>{
+    app.use(express.static(path.resolve(__dirname,'../public')));
+    res.sendFile(path.resolve(__dirname,'../public',"index.html"));
+});
+
 // middlewares
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
 
 module.exports = app;
